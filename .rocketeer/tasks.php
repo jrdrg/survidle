3f2e8560d503ec518bf6b820a6cd895f2f77954c
Maxime Fabre <ehtnam6@gmail.com>
2015-01-03 12:59:25 +0100

    Missing flag

--- .rocketeer/tasks.php
+++ .rocketeer/tasks.php
@@ -3,7 +3,7 @@ use Rocketeer\Facades\Rocketeer;
 
 $listeners = [
     'node_modules/.bin/tsd update --overwrite',
-    'node_modules/.bin/bower install',
+    'node_modules/.bin/bower install --allow-root',
     'node_modules/.bin/grunt',
 ];
 
