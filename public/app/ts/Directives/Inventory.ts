57dd9d407eda2b06880e2d3377079e903768ec79
Maxime Fabre <ehtnam6@gmail.com>
2015-01-03 13:06:56 +0100

    Make dropped items be dropped on the cell the player is in

--- public/app/ts/Directives/Inventory.ts
+++ public/app/ts/Directives/Inventory.ts
@@ -29,18 +29,21 @@ module Directives {
 		 */
 		isPlayer: boolean;
 
-		constructor(public game: Services.Game) {
+		constructor(public game: Services.Game, public items: Services.ItemsFactory) {
 		}
 
 		/**
 		 * When an item is clicked
 		 */
 		onItem = function (itemKey: string) {
+			var item = this.items.getItemByKey(itemKey);
+
 			if (this.isPlayer) {
-				this.game.player.drop(itemKey);
-			} else if (this.game.getScenery().canGather(itemKey)) {
-				this.game.getScenery().gather(itemKey);
-				this.game.stages['gather' + itemKey.title()] = true;
+				this.game.player.drop(item.key);
+				this.game.world.getPlayerCell().add(item);
+			} else if (this.game.getScenery().canGather(item.key)) {
+				this.game.getScenery().gather(item.key);
+				this.game.stages['gather' + item.key.title()] = true;
 			}
 		};
 
@@ -52,11 +55,13 @@ module Directives {
 
 			$scope.Math = Math;
 			$scope.action = this.onItem.bind(this);
+			$scope.label = this.isPlayer ? 'Inventory' : 'Zone';
 			$scope.tooltip = this.isPlayer ? 'Click to drop' : 'Gather';
+			$scope.isPlayer = this.isPlayer;
 		};
 
-		static instance(game: Services.Game) {
-			return new Inventory(game);
+		static instance(game: Services.Game, items: Services.ItemsFactory) {
+			return new Inventory(game, items);
 		}
 
 	}
